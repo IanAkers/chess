33724780353343a6282be8f5f5329114b0375d1d
Ian+Peter <ian+peter@appacademy.io>
2015-06-02 15:04:30 -0400

    Add all populate_piece pethods

--- board.rb
+++ board.rb
@@ -1,5 +1,10 @@
 require_relative 'piece.rb'
 require_relative 'pawn.rb'
+require_relative 'rook.rb'
+require_relative 'knight.rb'
+require_relative 'bishop.rb'
+require_relative 'king.rb'
+require_relative 'queen.rb'
 require 'byebug'
 
 class Board
@@ -21,11 +26,25 @@ class Board
 
 def initialize
   @grid = Array.new(8) {Array.new(8)}
-
 end
 
-  
+  def [](input)
+    input = input.to_s unless input.is_a?(String)
+    array = input.split("")
+    row = array[0].ord - 97
+    col = (array[1].to_i - 1)
+    @grid[row][col]
+  end
 
+  def populate_board
+    populate_pawns
+    populate_rooks
+    populate_knights
+    populate_bishops
+    populate_kings
+    populate_queens
+    self
+  end
 
 
   def populate_pawns
@@ -41,5 +60,69 @@ end
 
   end
 
+  def populate_rooks
+    ROOKS.each_with_index do |color_array, index|
+      color_array.each do |pos|
+        if index == 0
+          grid[pos[0], pos[1]] = Rook.new(pos[0], pos[1], :black, self)
+        else
+          grid[pos[0], pos[1]] = Rook.new(pos[0], pos[1], :white, self)
+        end
+      end
+    end
+
+  end
+
+  def populate_knights
+    KNIGHTS.each_with_index do |color_array, index|
+      color_array.each do |pos|
+        if index == 0
+          grid[pos[0], pos[1]] = Knight.new(pos[0], pos[1], :black, self)
+        else
+          grid[pos[0], pos[1]] = Knight.new(pos[0], pos[1], :white, self)
+        end
+      end
+    end
+
+  end
+
+  def populate_bishops
+    BISHOPS.each_with_index do |color_array, index|
+      color_array.each do |pos|
+        if index == 0
+          grid[pos[0], pos[1]] = Bishop.new(pos[0], pos[1], :black, self)
+        else
+          grid[pos[0], pos[1]] = Bishop.new(pos[0], pos[1], :white, self)
+        end
+      end
+    end
+
+  end
+
+  def populate_queens
+    QUEENS.each_with_index do |color_array, index|
+      color_array.each do |pos|
+        if index == 0
+          grid[pos[0], pos[1]] = Queen.new(pos[0], pos[1], :black, self)
+        else
+          grid[pos[0], pos[1]] = Queen.new(pos[0], pos[1], :white, self)
+        end
+      end
+    end
+
+  end
+
+  def populate_kings
+    KINGS.each_with_index do |color_array, index|
+      color_array.each do |pos|
+        if index == 0
+          grid[pos[0], pos[1]] = King.new(pos[0], pos[1], :black, self)
+        else
+          grid[pos[0], pos[1]] = King.new(pos[0], pos[1], :white, self)
+        end
+      end
+    end
+
+  end
 
 end
