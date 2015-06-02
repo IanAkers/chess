8faa732bf7266eeef660076ab00c6927a7d4c1fa
Ian+Peter <ian+peter@appacademy.io>
2015-06-02 17:07:13 -0400

    Refactor slidingpiece.rb

--- slidingpiece.rb
+++ slidingpiece.rb
@@ -2,21 +2,25 @@ require_relative "piece.rb"
 require 'byebug'
 class SlidingPiece < Piece
 
-  def moves
+  def all_moves
 
-    sub_results = []
+    results = []
     directions = self.move_dirs
     directions.each do |direction, delta|
 
       1.upto(8) do |idx|
         new_row = (row + (delta[0]*idx))
         new_col = col + (delta[1]*idx)
-        sub_results << [new_row, new_col]
+        results << [new_row, new_col]
       end
 
     end
+    results
+  end
+
+  def move_filter(moves)
     results = []
-    sub_results.each do |position|
+    moves.each do |position|
       current_row = position[0]
       current_col = position[1]
       inside_board = current_row.between?(0,7) && current_col.between?(0,7)
@@ -25,6 +29,12 @@ class SlidingPiece < Piece
         results << position if inside_board && not_original_position
     end
     results
+
+  end
+
+  def moves
+    move_filter(self.all_moves)
+
   end
 
 end
