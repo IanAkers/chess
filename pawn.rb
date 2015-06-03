16cef66098b23e5a3ce35887828d0c620e1da185
Ian+Peter <ian+peter@appacademy.io>
2015-06-03 11:27:42 -0400

    Add all appropriate move directions for pawns based on color.

--- pawn.rb
+++ pawn.rb
@@ -2,5 +2,26 @@ require_relative "piece.rb"
 
 class Pawn < Piece
 
+#direciton depends on color
+
+#can move 2 spaces in first move
+
+#moves diagonal to take a piece
+
+def move_dirs
+
+  if color ==  :black
+      {:down => [1,0], :double_down => [2,0],
+      :down_right => [1,1], :down_left => [1,-1]}
+
+  else
+      {:up => [-1,0], :double_up => [-2,0],
+      :up_right => [-1,1], :up_left => [-1, -1]}
+
+  end
+end
+
+
+
 
 end
