35b56f3d4a534745303f9dd5dbef21d4c91cb4c8
Ian+Peter <ian+peter@appacademy.io>
2015-06-03 12:04:51 -0400

    Implement helper method for 'double moves' for pawn class.

--- pawn.rb
+++ pawn.rb
@@ -21,7 +21,48 @@ def move_dirs
   end
 end
 
+def moves
+  results = Hash.new()
+  directions = self.move_dirs
+  directions.each do |direction, delta|
 
 
+    new_row = row + (delta[0])
+    new_col = col + (delta[1])
+    unless friend_occupied?(new_row, new_col)
+
+    #conditional for "double" moves
+      if delta[0] != (1 || -1)
+        results[direction] = double_moves(new_row, new_col)
+       end
+     end
+
+     #conditional for up/down moves
+
+
+
+     #conditional for diagonal moves
+
+   end
+
+   results
+
+end
+
+  def double_moves(new_row, new_col)
+
+    results = []
+    unless enemy_occupied?(new_row, new_col)
+      #unless black piece has moved
+      if self.color == :black && self.row == 1
+        results += [new_row, new_col]
+      end
+      #unless white piece has moved
+      if self.color == :white && self.row == 6
+        results += [new_row, new_col]
+      end
+    end
+      results
+  end
 
 end
